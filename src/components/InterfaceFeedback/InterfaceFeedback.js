dadfc72863fef9943870cc1b7f3c1638494fe5a2
VadymMurzin <prometey736@gmail.com>
2023-07-13 17:43:54 +0200

    first commit

---
+++ src/components/InterfaceFeedback/InterfaceFeedback.js
@@ -0,0 +1,10 @@
+import css from './interfaceFeedback.module.css';
+import PropTypes from 'prop-types';
+
+export default function InterfaceFeedback () {
+    return (
+      <section className={css.statistics}>
+        
+      </section>
+    );
+  }
